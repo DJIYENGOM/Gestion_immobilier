6faa665346d24cd15c3312a7778138c4d0232bcc
Djiyé NGOM <ngomdjiye@gmail.com>
2023-11-17 12:59:54 +0000

    1er commit

---
+++ resources/views/biens/liste.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.nav')
+@section('contenue')
+<div class="container">
+    <div class="row">
+        @foreach($biens as $bien)
+        <div class="col-12 col-md-3 mt-4">
+            <div class="card">
+                <div class="card-header">
+                    Article
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
+                    <rect width="100%" height="100%" fill="#868e96"></rect>
+                    <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                </svg>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <h5 class="card-title"></h5>
+                    <h5 class="card-title"></h5>
+                    <a href="#" class="btn btn-primary">Commenter</a>
+                    <a href="#" class="btn btn-primary">Voir Plus</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
